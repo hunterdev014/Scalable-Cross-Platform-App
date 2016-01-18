8608d3c7405976628c3659cdd2bb01184c740d00
hunterdev014 <mmarkusschuster87@gmail.com>
2016-01-18 08:02:53 +0100

    Slightly terser code splitting

--- webpack/server.prod.js
+++ webpack/server.prod.js
@@ -1,19 +1,11 @@
 /* eslint no-console:0 */
 // Gets called when running npm run serve
 
-const path = require('path');
 const webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
 const config = require('./webpack.prod.babel');
 const ngrok = require('ngrok');
-const chalk = require('chalk');
 
-console.log(
-  chalk.bold('Options:\n') +
-  chalk.gray('-----------------------------------\n') +
-  chalk.cyan('Source: ') + path.join(__dirname, '..', 'app') + '\n' +
-  chalk.gray('-----------------------------------\n')
-);
 console.log('Starting server from build folder...');
 
 new WebpackDevServer(webpack(config), { // Start a server
@@ -30,11 +22,10 @@ new WebpackDevServer(webpack(config), { // Start a server
     console.log('Server started');
     ngrok.connect(3000, (innerErr, url) => {
       if (innerErr) {
-        console.log(chalk.red('ERROR\n' + innerErr));
+        console.log('ERROR\n' + innerErr);
       }
       console.log('Tunnel initialised');
-      console.log('\nYour app is available at ' + chalk.magenta(url));
-      console.log(chalk.blue('\nPress ' + chalk.italic('CTRL-C') + ' to stop'));
+      console.log('\nYour app is available at ' + url + '!');
     });
   }
 });
