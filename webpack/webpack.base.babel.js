dde2c3934d6189288b33fe5511fe6d2ae97d9ce1
hunterdev014 <mmarkusschuster87@gmail.com>
2016-01-29 22:27:28 +0100

    Fix shouldComponentUpdate

--- webpack/webpack.base.babel.js
+++ webpack/webpack.base.babel.js
@@ -52,7 +52,6 @@ module.exports = (options) => {
         '.react.js'
       ]
     },
-    devtool: options.devtool,
     target: 'web', // Make web variables accessible to webpack, e.g. window
     stats: false, // Don't show stats in the console
     progress: true
