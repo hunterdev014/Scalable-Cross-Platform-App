8ea23a268389f2b11ef02c2c737b16e99f0236f1
hunterdev014 <mmarkusschuster87@gmail.com>
2016-01-30 10:57:38 +0100

    Fix package.json

--- webpack/webpack.dev.babel.js
+++ webpack/webpack.dev.babel.js
@@ -41,7 +41,5 @@ module.exports = require('./webpack.base.babel')({
   // Tell babel that we want to hot-reload
   babelQuery: {
     presets: ['react-hmre']
-  },
-  // Emit a source map for easier debugging
-  devtool: 'inline-source-map'
+  }
 });
