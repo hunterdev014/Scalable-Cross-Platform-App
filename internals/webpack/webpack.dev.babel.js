c17e1ff281f7dda6a53dcf1526c9558483b5c1b8
hunterdev014 <mmarkusschuster87@gmail.com>
2016-03-01 13:27:18 +0100

    Remove relative paths

--- internals/webpack/webpack.dev.babel.js
+++ internals/webpack/webpack.dev.babel.js
@@ -19,13 +19,8 @@ module.exports = require('./webpack.base.babel')({
   entry: [
     'webpack-dev-server/client?http://localhost:' + port, // Needed for hot reloading
     'webpack/hot/only-dev-server',
-    path.join(__dirname, '../..', 'app/app.js'), // Start with js/app.js
+    path.join(process.cwd(), 'app/app.js'), // Start with js/app.js
   ],
-  // Don't use hashes in dev mode for better performance
-  output: {
-    filename: '[name].js',
-    chunkFilename: '[name].chunk.js',
-  },
   // Load the CSS in a style tag in development
   cssLoaders: 'style-loader!css-loader?modules&importLoaders=1&sourceMap!postcss-loader',
   // Process the CSS with PostCSS
