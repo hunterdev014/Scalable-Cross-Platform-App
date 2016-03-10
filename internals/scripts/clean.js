229f4eddbcff557e86396126344bca85ceef0474
hunterdev014 <mmarkusschuster87@gmail.com>
2016-03-10 09:50:15 -0800

    Changed to reflect suggestion in react-router-redux#314, which removes interim if key does not exist check

--- internals/scripts/clean.js
+++ internals/scripts/clean.js
@@ -27,27 +27,35 @@ rm('-rf', 'app/components/*');
 // Cleanup containers folder
 rm('-rf', 'app/containers/*');
 mkdir('app/containers/App');
-mkdir('app/components/NotFoundPage');
-mkdir('app/components/HomePage');
+mkdir('app/containers/NotFoundPage');
+mkdir('app/containers/HomePage');
 cp('internals/templates/appContainer.js', 'app/containers/App/index.js');
-cp('internals/templates/notFoundPage.js', 'app/components/NotFoundPage/index.js');
-cp('internals/templates/homePage.js', 'app/components/HomePage/index.js');
+cp('internals/templates/notFoundPage.js', 'app/containers/NotFoundPage/index.js');
+cp('internals/templates/homePage.js', 'app/containers/HomePage/index.js');
 
 // Cleanup sagas folder
 rm('-rf', 'app/sagas/*');
 cp('internals/templates/sagas.js', 'app/sagas/index.js');
 
-// Cleanup selectors folder
+// Cleanup selectors folder, leave selectLocationSelection(.test).js
 rm('-rf', 'app/selectors/*');
+mkdir('-p', 'app/selectors/tests');
+cp('internals/templates/selectLocationSelector.js',
+  'app/selectors/selectLocationSelector.js');
+cp('internals/templates/selectLocationSelector.test.js',
+  'app/selectors/tests/selectLocationSelector.test.js');
 
 // Delete utils folder
 rm('-rf', 'app/utils');
 
-// Replace app.js, index.html, rootReducer.js and routes.js
+// Replace app.js, index.html, rootReducer.js, routeReducer.js, routes.js and store.js
 cp('internals/templates/app.js', 'app/app.js');
 cp('internals/templates/index.html', 'app/index.html');
 cp('internals/templates/rootReducer.js', 'app/rootReducer.js');
+cp('internals/templates/routeReducer.js', 'app/routeReducer.js');
 cp('internals/templates/routes.js', 'app/routes.js');
+cp('internals/templates/store.js', 'app/store.js');
+
 
 // Remove the templates folder
 rm('-rf', 'internals/templates');
