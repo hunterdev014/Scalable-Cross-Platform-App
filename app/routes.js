c5a2a4bf4b37187dfa21c05d5d5b95fa9a1bfe4a
hunterdev014 <mmarkusschuster87@gmail.com>
2016-04-05 23:50:15 +0300

    add packageMains resolve option

--- app/routes.js
+++ app/routes.js
@@ -1,32 +1,49 @@
+/* istanbul ignore next */
+
 // These are the pages you can go to.
 // They are all wrapped in the App component, which should contain the navbar etc
 // See http://blog.mxstbr.com/2016/01/react-apps-with-pages for more information
 // about the require.ensure code splitting business
 import { injectAsyncReducer } from './store';
 
+function errorLoading(err) {
+  console.error('Dynamic page loading failed', err);
+}
+
+function loadModule(cb) {
+  return (module) => cb(null, module.default);
+}
+
+function loadReducer(store, name) {
+  return (module) => injectAsyncReducer(store, name, module.default);
+}
+
 export default function createRoutes(store) {
   return [
     {
       path: '/',
-      getComponent: function get(location, cb) {
-        require.ensure([], (require) => {
-          injectAsyncReducer(store, 'home', require('HomePage/reducer').default);
-          cb(null, require('HomePage').default);
-        }, 'HomePage');
+      getComponent(location, cb) {
+        System.import('HomePage/reducer')
+          .then(loadReducer(store, 'home'))
+          .catch(errorLoading);
+
+        System.import('HomePage')
+          .then(loadModule(cb))
+          .catch(errorLoading);
       },
     }, {
       path: '/features',
-      getComponent: function get(location, cb) {
-        require.ensure([], (require) => {
-          cb(null, require('FeaturePage').default);
-        }, 'FeaturePage');
+      getComponent(location, cb) {
+        System.import('FeaturePage')
+          .then(loadModule(cb))
+          .catch(errorLoading);
       },
     }, {
       path: '*',
-      getComponent: function get(location, cb) {
-        require.ensure([], (require) => {
-          cb(null, require('NotFoundPage').default);
-        }, 'NotFoundPage');
+      getComponent(location, cb) {
+        System.import('NotFoundPage')
+          .then(loadModule(cb))
+          .catch(errorLoading);
       },
     },
   ];
