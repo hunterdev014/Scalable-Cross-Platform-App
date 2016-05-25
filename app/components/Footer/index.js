3cfc101e85aaef6c2bf93f812038af4647c50394
hunterdev014 <mmarkusschuster87@gmail.com>
2016-05-25 17:45:09 -0500

    Fix wrong command on CLI readme for pagespeed

--- app/components/Footer/index.js
+++ app/components/Footer/index.js
@@ -2,33 +2,15 @@ import React from 'react';
 
 import A from 'components/A';
 import styles from './styles.css';
-import { FormattedMessage } from 'react-intl';
 
 function Footer() {
   return (
     <footer className={styles.footer}>
       <section>
-        <p>
-          <FormattedMessage
-            id="boilerplate.components.Footer.license.message"
-            defaultMessage={`
-              This project is licensed under the MIT license.
-            `}
-          />
-        </p>
+        <p>This project is licensed under the MIT license.</p>
       </section>
       <section>
-        <p>
-          <FormattedMessage
-            id="boilerplate.components.Footer.author.message"
-            defaultMessage={`
-              Made with love by {author}.
-            `}
-            values={{
-              author: <A href="https://twitter.com/mxstbr">Max Stoiber</A>,
-            }}
-          />
-        </p>
+        <p>Made with love by <A href="https://twitter.com/mxstbr">Max Stoiber</A>.</p>
       </section>
     </footer>
   );
