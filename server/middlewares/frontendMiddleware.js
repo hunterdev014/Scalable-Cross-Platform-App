6fef2d35909242d4776c97e6ed28d59cdff2deec
hunterdev014 <mmarkusschuster87@gmail.com>
2016-05-27 18:15:17 +0800

    Added intl chunk loading polyfill as per @justingreenberg's chunking suggestion. Changed config file to il8n.js file and took out the babel polling config.

--- server/middlewares/frontendMiddleware.js
+++ server/middlewares/frontendMiddleware.js
@@ -12,6 +12,7 @@ const addDevMiddlewares = (app, options) => {
     noInfo: true,
     publicPath: options.output.publicPath,
     silent: true,
+    watchOptions: options.watchOptions,
   });
 
   app.use(middleware);
