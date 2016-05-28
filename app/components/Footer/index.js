2b5029b29c54d05cd52f8d3b2c624c6d20c7c15f
hunterdev014 <mmarkusschuster87@gmail.com>
2016-05-28 13:35:47 -0400

    Update to more performant source mapping

--- app/components/Footer/index.js
+++ app/components/Footer/index.js
@@ -1,27 +1,16 @@
 import React from 'react';
 
-import messages from './messages';
 import A from 'components/A';
 import styles from './styles.css';
-import { FormattedMessage } from 'react-intl';
 
 function Footer() {
   return (
     <footer className={styles.footer}>
       <section>
-        <p>
-          <FormattedMessage {...messages.licenseMessage} />
-        </p>
+        <p>This project is licensed under the MIT license.</p>
       </section>
       <section>
-        <p>
-          <FormattedMessage
-            {...messages.authorMessage}
-            values={{
-              author: <A href="https://twitter.com/mxstbr">Max Stoiber</A>,
-            }}
-          />
-        </p>
+        <p>Made with love by <A href="https://twitter.com/mxstbr">Max Stoiber</A>.</p>
       </section>
     </footer>
   );
