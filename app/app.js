c333000772846d0e3976ec8e1792c784d0d7536a
hunterdev014 <mmarkusschuster87@gmail.com>
2016-06-11 12:43:16 +0200

    fix components generator

--- app/app.js
+++ app/app.js
@@ -66,7 +66,21 @@ ReactDOM.render(
       render={
         // Scroll to top when going to a new page, imitating default browser
         // behaviour
-        applyRouterMiddleware(useScroll())
+        applyRouterMiddleware(
+          useScroll(
+            (prevProps, props) => {
+              if (!prevProps || !props) {
+                return true;
+              }
+
+              if (prevProps.location.pathname !== props.location.pathname) {
+                return [0, 0];
+              }
+
+              return true;
+            }
+          )
+        )
       }
     />
   </Provider>,
