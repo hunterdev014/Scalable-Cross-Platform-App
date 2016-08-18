7fb17546047d8a38ccc08a537bec5ba5bcfe0439
hunterdev014 <mmarkusschuster87@gmail.com>
2016-08-19 00:35:18 +0300

    fix(internal): indent <Helmet> correctly in container's `index.js.hbs` template (#883)

--- internals/scripts/setup.js
+++ internals/scripts/setup.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 
-var shell = require('shelljs');
 var exec = require('child_process').exec;
 var path = require('path');
 var fs   = require('fs');
@@ -11,11 +10,12 @@ var readline = require('readline');
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
 
+var dir = process.cwd();
+
 process.stdout.write('\n');
 var interval = animateProgress('Cleaning old repository');
 process.stdout.write('Cleaning old repository');
-
-cleanRepo(function () {
+cleanRepo(dir, function () {
   clearInterval(interval);
   process.stdout.write('\nInstalling dependencies... (This might take a while)');
   setTimeout(function () {
@@ -34,7 +34,7 @@ cleanRepo(function () {
       process.stdout.write('\n');
       interval = animateProgress('Initialising new repository');
       process.stdout.write('Initialising new repository');
-      initGit(function () {
+      initGit(dir, function () {
         clearInterval(interval);
         process.stdout.write('\nDone!');
         process.exit(0);
@@ -46,15 +46,14 @@ cleanRepo(function () {
 /**
  * Deletes the .git folder in dir
  */
-function cleanRepo(callback) {
-  shell.rm('-rf', '.git/');
-  addCheckMark(callback);
+function cleanRepo(dir, callback) {
+  exec('rm -Rf .git/', addCheckMark.bind(null, callback));
 }
 
 /**
  * Initializes git again
  */
-function initGit(callback) {
+function initGit(dir, callback) {
   exec('git init && git add . && git commit -m "Initial commit"', addCheckMark.bind(null, callback));
 }
 
