a8a504318280726db998c5b4459c0f055549a275
hunterdev014 <mmarkusschuster87@gmail.com>
2018-11-27 18:25:51 +0200

    Update issue template and issue-close-app

--- internals/templates/containers/App/selectors.js
+++ internals/templates/containers/App/selectors.js
@@ -3,9 +3,8 @@ import { createSelector } from 'reselect';
 const selectRouter = state => state.get('router');
 
 const makeSelectLocation = () =>
-  createSelector(
-    selectRouter,
-    routerState => routerState.get('location').toJS(),
+  createSelector(selectRouter, routerState =>
+    routerState.get('location').toJS(),
   );
 
 export { makeSelectLocation };
